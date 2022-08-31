8ca56e12bcc8c166258d4202c36ad8065b85fa92
tobiasmartinez <tobiassmartinez18@gmail.com>
2022-08-30 23:56:09 -0300

    termine contador

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,26 +1,30 @@
 
-import { useState } from 'react';
+import { isDisabled } from '@testing-library/user-event/dist/utils';
+import react,{ useState } from 'react';
 
-const ItemCount = () => {
-    function Producto ({stock,initial,onAdd}){
-        const {contador,setContador}=useState(initial);
-        const suma = () =>{
-            setContador(contador + 1);
-        }
-        const resta = () =>{
-            setContador(contador -1);
-        }
-        return(
-            <div>
-                <h3>Stock de remera azul</h3>
-                <p>La cantidad de stock que tenemos es {stock}</p>
-                <p>{contador}</p>
-                <button onClick={suma}>+</button>
-                <button onClick={resta}>-</button>
-            </div>
-        );
+
+const ItemCount = ({stock,initial,onAdd}) =>{
+    const [contador,setContador]=useState(1);
+    const resta = () => {
+        setContador(contador - 1);
+
     }
+    const suma = () => {
+        setContador(contador + 1);
+    }
+    return(
+       
+        <div>
+            <h4>Stock de remera azul</h4>
+            <h5>La cantidad de stock que tenemos es de {stock}</h5>
+            <p>{contador}</p>
+            <button disabled={contador >= stock} onClick={suma}>+</button>
+            <button disabled={contador <= initial} onClick={resta}>-</button>
+            <div>
+                <button disabled={stock <= 0} onClick={()=> onAdd(contador)}> Agregar al Carrito</button>
+            </div>
+        </div>
+    );
 }
-
 export default ItemCount;
 
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,13 +1,18 @@
 import react from 'react'
-import ItemCount from './ItemCount';
+import ItemCount from '../components/ItemCount.js';
 
 
 const itemListContainer = ({nombresProductos,contenido}) => {
+
+    const onAdd = (quantity ) =>{
+        console.log(`Compraste ${quantity} unidades`);
+    }
+
     return(
         <div>
             <h3>{nombresProductos}</h3>
             <p>{contenido}</p>
-            <ItemCount stock='10' initial='1'/>
+            <ItemCount stock='10' initial='1' onAdd={onAdd}/>
         </div>
         
     );
